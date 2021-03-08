cc76b9bfcea018bc45ec6012801aa4690cea6b3c
talentedasian <lucenaalmirjames123@gmail.com>
2021-03-08 17:12:35 +0800

    added jwt not found test case

--- src/test/java/com/example/forum_4_stupid/DemoApplicationTests.java
+++ src/test/java/com/example/forum_4_stupid/DemoApplicationTests.java
@@ -1,19 +1,7 @@
 package com.example.forum_4_stupid;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import com.example.forum_4_stupid.controller.UserController;
-import com.example.forum_4_stupid.service.UserService;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -21,19 +9,5 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Autowired
-	private MockMvc mockMvc;
-	@MockBean
-	private UserService userService;
-	@MockBean
-	private UserController userController;
-	
-	@Test
-	public void assertThatSignupControllerIsSaved() throws URISyntaxException, Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get(new URI("/api/user/userById/1")))
-		.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().is(403));
-	}
-	
 	
 }
--- src/test/java/com/example/forum_4_stupid/UserControllerTest.java
+++ src/test/java/com/example/forum_4_stupid/UserControllerTest.java
@@ -21,7 +21,7 @@ import com.example.forum_4_stupid.repository.UsersRepository;
 import com.example.forum_4_stupid.service.UserService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = UserController.class, AuthenticationController.class)
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
 
 	@Autowired
@@ -35,6 +35,7 @@ public class UserControllerTest {
 	
 	private String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnYWdvIiwiZXhwIjoxNjE1MTYyNjIxLCJqdGkiOiIxIn0.vmJSDU9S4Kznv6tGYRjcJdcGif8-y01jv_ktC0Y2cpk";
 	private String expectedJwtResponseWhenExpired = "{\"reason\":\"Server might have restarted or JWT was tampered\",\"code\":\"401\",\"err\":\"Invalid Signature of JWT\"}";
+	private String expectedJwtResponseNotFound = "{\"code\":\"401\",\"err\":\"No JWT Found on Authorization Header\"}";
 	
 	@Test
 	public void assertThatAccessingProtectedResourceReturnForbidden() throws URISyntaxException, Exception {
@@ -51,6 +52,14 @@ public class UserControllerTest {
 		.andExpect(MockMvcResultMatchers.status().is(401))
 		.andExpect(MockMvcResultMatchers.content().json(expectedJwtResponseWhenExpired));
 		
+	}
+	
+	@Test
+	public void assertThatJwtReturnNoJwt() throws URISyntaxException, Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get(new URI("/api/user/userById/1")))
+		.andDo(MockMvcResultHandlers.print())
+		.andExpect(MockMvcResultMatchers.status().is(401))
+		.andExpect(MockMvcResultMatchers.content().json(expectedJwtResponseNotFound));
 	}
 	
 }
