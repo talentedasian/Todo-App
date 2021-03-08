9c89cde1d8f0821e0d9a894ccd1ba79239eacdbf
talentedasian <lucenaalmirjames123@gmail.com>
2021-03-08 15:33:26 +0800

    added test case for jwt ifilters

---
+++ src/test/java/com/example/forum_4_stupid/UserControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.forum_4_stupid;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.forum_4_stupid.controller.UserController;
+import com.example.forum_4_stupid.repository.UsersRepository;
+import com.example.forum_4_stupid.service.UserService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UserController.class, AuthenticationController.class)
+public class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private UserService userService;
+	@MockBean
+	private UserController userController;
+	@MockBean
+	private UsersRepository repo;
+	
+	private String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnYWdvIiwiZXhwIjoxNjE1MTYyNjIxLCJqdGkiOiIxIn0.vmJSDU9S4Kznv6tGYRjcJdcGif8-y01jv_ktC0Y2cpk";
+	private String expectedJwtResponseWhenExpired = "{\"reason\":\"Server might have restarted or JWT was tampered\",\"code\":\"401\",\"err\":\"Invalid Signature of JWT\"}";
+	
+	@Test
+	public void assertThatAccessingProtectedResourceReturnForbidden() throws URISyntaxException, Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get(new URI("/api/user/userById/1")))
+		.andDo(MockMvcResultHandlers.print())
+		.andExpect(MockMvcResultMatchers.status().is(401));
+	}
+	
+	@Test
+	public void assertThatWExpiredJwt() throws URISyntaxException, Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get(new URI("/api/user/userById/1"))
+				.header("Authorization", jwt))
+		.andDo(MockMvcResultHandlers.print())
+		.andExpect(MockMvcResultMatchers.status().is(401))
+		.andExpect(MockMvcResultMatchers.content().json(expectedJwtResponseWhenExpired));
+		
+	}
+	
+}
