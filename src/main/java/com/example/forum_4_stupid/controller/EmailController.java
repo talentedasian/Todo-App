65532fc4b0f94243d33f93f86ab10e58c856cca7
talentedasian <lucenaalmirjames123@gmail.com>
2021-03-12 12:47:25 +0800

    added hateoas on email endpoints

--- src/main/java/com/example/forum_4_stupid/controller/EmailController.java
+++ src/main/java/com/example/forum_4_stupid/controller/EmailController.java
@@ -1,7 +1,10 @@
 package com.example.forum_4_stupid.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.forum_4_stupid.dto.EmailDTO;
 import com.example.forum_4_stupid.dto.EmailRequest;
 import com.example.forum_4_stupid.dtoMapper.EmailDtoMapper;
+import com.example.forum_4_stupid.hateoas.EmailRepresentationalModel;
 import com.example.forum_4_stupid.service.EmailService;
 
 @RestController
@@ -32,10 +36,20 @@ public class EmailController {
 	}
 	
 	@GetMapping("/{owner_id}")
-	public ResponseEntity<List<EmailDTO>> getEmail(@PathVariable String owner_id) {
-		List<EmailDTO> response = emailDtoMapper.getAllEmailByUsersId(Integer.parseInt(owner_id));
+	public ResponseEntity<List<EmailRepresentationalModel>> getEmail(@PathVariable String owner_id) {
+		List<EmailDTO> email = emailDtoMapper.getAllEmailByUsersId(Integer.parseInt(owner_id));
+		List<EmailRepresentationalModel> emailResponse = new ArrayList<>();
 		
-		return new ResponseEntity<List<EmailDTO>>(response, HttpStatus.OK);
+		for (EmailDTO emailDTO : email) {
+			EmailRepresentationalModel emailModel = new EmailRepresentationalModel();
+			emailModel.setId(emailDTO.getId().toString());
+			emailModel.setEmail(emailDTO.getEmail());
+			emailModel.add(Link.of("http://localhost:8080/api/email/" + owner_id));
+			
+			emailResponse.add(emailModel)
+;		}
+		
+		return new ResponseEntity<List<EmailRepresentationalModel>>(emailResponse, HttpStatus.OK);
 	}
 	
 }
