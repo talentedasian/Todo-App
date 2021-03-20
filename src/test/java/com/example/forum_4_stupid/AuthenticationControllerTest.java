81ef082aeef498c855b36a0ad47a07a11ae2879f
talentedasian <lucenaalmirjames123@gmail.com>
2021-03-20 15:41:26 +0800

    stubbing mockito methods successful

--- src/test/java/com/example/forum_4_stupid/AuthenticationControllerTest.java
+++ src/test/java/com/example/forum_4_stupid/AuthenticationControllerTest.java
@@ -4,53 +4,38 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Instant;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.when;
-import org.mockito.hamcrest.MockitoHamcrest;
-import org.mockito.internal.MockitoCore;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.example.forum_4_stupid.config.SecurityConfig;
 import com.example.forum_4_stupid.controller.AuthenticationController;
 import com.example.forum_4_stupid.dto.LoginRequest;
 import com.example.forum_4_stupid.dto.RegisterRequest;
 import com.example.forum_4_stupid.dto.UserDTO;
 import com.example.forum_4_stupid.dtoMapper.UserDtoMapper;
-import com.example.forum_4_stupid.exceptions.EmailAlreadyExistsException;
 import com.example.forum_4_stupid.hateoas.UserDTOAssembler;
 import com.example.forum_4_stupid.model.Users;
 import com.example.forum_4_stupid.repository.UsersRepository;
 import com.example.forum_4_stupid.service.AuthService;
 import com.example.forum_4_stupid.service.JwtProvider;
 import com.example.forum_4_stupid.service.UserService;
-import com.example.forum_4_stupid.utility.NestedDTOAssembler;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = AuthenticationController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
@@ -67,32 +52,39 @@ public class AuthenticationControllerTest {
 	private UserDtoMapper mapper;
 	@MockBean
 	private AuthService auth;
-	@Autowired
-	private WebApplicationContext context; 
 	@MockBean
 	private UserService userService;
+	@Autowired
+	private WebApplicationContext context;
 	
-	private final String expectedSignupBodyResponse = "{\n"
-			+ "    \"id\": 1,\n"
-			+ "    \"username\": \"test\",\n"
-			+ "    \"totalEmails\": 0,\n"
-			+ "    \"totalTodos\": 0,\n"
-			+ "    \"_links\": {\n"
-			+ "        \"userById\": {\n"
-			+ "            \"href\": \"http://localhost:8080/api/user/userById/1\"\n"
-			+ "        },\n"
-			+ "        \"userByUsername\": {\n"
-			+ "            \"href\": \"http://localhost:8080/api/user/userByUsername?username=test\"\n"
-			+ "        }\n"
-			+ "    }\n"
-			+ "}";
+	@Before
+	public void setUpMockMvc() {
+		mockMvc = MockMvcBuilders.standaloneSetup(context.getBean(AuthenticationController.class))
+				.build();
+	}
+	
+	@Before
+	public void setUpRepo() {
+		Users user = new Users(1, "test", Instant.now(), "testpassword", true,null, null);
+		context.getBean(UsersRepository.class).save(user);
+	}
 	
 	@Test
 	public void assertThatSignUpReturnStatusCreated() throws URISyntaxException, Exception {
+		var registerRequest = new RegisterRequest("test", "testpassword");
+		var userDTO = new UserDTO();
+		userDTO.setId(1);
+		userDTO.setUsername("test");
+		userDTO.setTotalEmails(0);
+		userDTO.setTotalTodos(0);
+		when(mapper.save(registerRequest)).thenReturn(userDTO);
+		EntityModel<UserDTO> ass = Mockito.spy(UserDTOAssembler.class).toModel(userDTO);
+		when(assembler.toModel(userDTO)).thenReturn(ass);
 		this.mockMvc.perform(MockMvcRequestBuilders.post(new URI("/auth/signup"))
 				.characterEncoding("utf-8")
 				.content("{\n\"username\": \"test\",\n\"password\": \"testpassword\"\n}")
 				.contentType(MediaType.APPLICATION_JSON))
+		.andDo(MockMvcResultHandlers.print())
 		.andExpect(MockMvcResultMatchers.status().isCreated())
 		.andExpect(MockMvcResultMatchers.content().contentType(org.springframework.hateoas.MediaTypes.HAL_JSON));
 		
@@ -111,16 +103,7 @@ public class AuthenticationControllerTest {
 				.headers(headers))
 		.andDo(MockMvcResultHandlers.print())
 		.andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(MockMvcResultMatchers.header().exists("Authorization"));
-		
+		.andExpect(MockMvcResultMatchers.header().exists("Authorization"));	
 	}
 	
-//	@Test
-//	public void assertThatFailedLoginShouldReturnForbidden() throws URISyntaxException, Exception {
-//		this.mockMvc.perform(MockMvcRequestBuilders.post(new URI("/auth/login"))
-//				.param("username", "test")
-//				.param("password", "wrongtestpassword"))
-//		.andDo(MockMvcResultHandlers.print())
-//		.andExpect(MockMvcResultMatchers.status().isForbidden());
-//	}
 }
---
+++ src/test/java/com/example/forum_4_stupid/UserControllerTest.java
@@ -0,0 +1,80 @@
+package com.example.forum_4_stupid;
+ 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Instant;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.forum_4_stupid.controller.UserController;
+import com.example.forum_4_stupid.dto.RegisterRequest;
+import com.example.forum_4_stupid.dto.UserDTO;
+import com.example.forum_4_stupid.dtoMapper.UserDtoMapper;
+import com.example.forum_4_stupid.hateoas.UserDTOAssembler;
+import com.example.forum_4_stupid.model.Users;
+import com.example.forum_4_stupid.repository.UsersRepository;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private UserDtoMapper mapper;
+	@MockBean
+	private UserDTOAssembler assembler;
+	@Autowired
+	private WebApplicationContext context;
+	@MockBean
+	private UserDetailsService service;
+	
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(context.getBean(UserController.class))
+				.build();
+	}
+	
+	@Before
+	public void setUpRepo() {
+		Users user = new Users(1, "test", Instant.now(), "testpassword", true,null, null);
+		context.getBean(UsersRepository.class).save(user);
+	}
+	
+	@Test
+	public void assertThatUserShouldReturnStatusOk() throws URISyntaxException, Exception {
+		var userDTO = new UserDTO();
+		userDTO.setId(1);
+		userDTO.setUsername("test");
+		userDTO.setTotalEmails(0);
+		userDTO.setTotalTodos(0);
+		when(mapper.getById(1)).thenReturn(userDTO);
+		EntityModel<UserDTO> ass = Mockito.spy(UserDTOAssembler.class).toModel(userDTO);
+		when(assembler.toModel(userDTO)).thenReturn(ass);
+		mockMvc.perform(MockMvcRequestBuilders.get(new URI("/api/user/userById/1"))
+				.content("utf-8"))
+		.andDo(MockMvcResultHandlers.print())
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+}
