cb04b061d1fe156ba3a51545e3c7b3ba36460f32
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-01 14:59:24 +0800

    tested spring validation

--- src/test/java/com/example/forum_4_stupid/integration/controller/AuthenticationControllerTest.java
+++ src/test/java/com/example/forum_4_stupid/integration/controller/AuthenticationControllerTest.java
@@ -101,7 +101,7 @@ public class AuthenticationControllerTest {
 		this.mockMvc.perform(MockMvcRequestBuilders.post(new URI("/auth/signup"))
 				.characterEncoding("utf-8")
 				.content("{\n\"username\": \"test\",\n\"password\": \"test\"\n}")
-				.contentType(MediaType.APPLICATION_JSON))
+				.contentType(MediaType.APPLICATION_JSON))w
 		.andExpect(MockMvcResultMatchers.status().isBadRequest());
 	}
 	
