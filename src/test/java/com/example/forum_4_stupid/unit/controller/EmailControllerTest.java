6fb48cae562551625580fe5baf5d0ee8d98eaef8
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-04 23:28:28 +0800

    setup for email controller unit tests

---
+++ src/test/java/com/example/forum_4_stupid/unit/controller/EmailControllerTest.java
@@ -0,0 +1,43 @@
+package com.example.forum_4_stupid.unit.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.forum_4_stupid.controller.EmailController;
+import com.example.forum_4_stupid.dto.EmailDTO;
+import com.example.forum_4_stupid.dto.UserDTO;
+import com.example.forum_4_stupid.dtoMapper.EmailDtoMapper;
+import com.example.forum_4_stupid.hateoas.EmailDTOAssembler;
+
+@ExtendWith(SpringExtension.class)
+public class EmailControllerTest {
+
+	private static EmailDTO emailDTO;
+	private static EmailController controller;
+	
+	@MockBean
+	private EmailDTOAssembler assembler;
+	@MockBean
+	private EmailDtoMapper mapper;
+	
+	@BeforeEach
+	public void setUp() {
+		var userDTO = new UserDTO();
+		userDTO.setId(1);
+		userDTO.setUsername("test");
+		userDTO.setTotalEmails(0);
+		userDTO.setTotalTodos(0);
+		emailDTO = new EmailDTO();
+		emailDTO.setId(1);
+		emailDTO.setEmail("test@gmail.com");
+		emailDTO.setUser(userDTO);
+	}
+	
+	@Test
+	public void verifyEmailDTOAssemblerCalled() {
+		
+	}
+}
