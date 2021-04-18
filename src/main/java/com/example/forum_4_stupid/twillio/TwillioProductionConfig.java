af3883d06b2b508c4dc705ce64ac6ae6cc393d38
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-18 13:26:20 +0800

    added a bean phone number for production

--- src/main/java/com/example/forum_4_stupid/twillio/TwillioProductionConfig.java
+++ src/main/java/com/example/forum_4_stupid/twillio/TwillioProductionConfig.java
@@ -3,10 +3,12 @@ package com.example.forum_4_stupid.twillio;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.twilio.Twilio;
+import com.twilio.type.PhoneNumber;
 
 @Configuration
 @Profile("production")
@@ -20,6 +22,11 @@ public class TwillioProductionConfig {
 	
 	@Value("${twillio.account.token: #{null}}")
 	private String TWILLIO_ACCOUNT_TOKEN;
+	
+	@Bean
+	public PhoneNumber phoneNumber() {
+		return new PhoneNumber(phoneNumber);
+	}
 
 	@PostConstruct
 	public void initializeTwillio() {
